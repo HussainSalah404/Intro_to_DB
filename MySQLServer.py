fd1966d8ea2340d4fd4f27861ae9b2960f52d2aa
HussainSalah404 <hussainsalah404@gmail.com>
2025-08-13 09:39:13 +0300

    added if the script contains the CREATE DATABASE statement for alxbookstore

--- MySQLServer.py
+++ MySQLServer.py
@@ -10,3 +10,11 @@ mydb = mysql.connector.connect(
 
 
 print("Database 'alx_book_store' created successfully!")
+
+with open("alx_book_store.sql", "r", encoding="utf-8") as f:
+    sql_content = f.read()
+
+if "CREATE DATABASE IF NOT EXISTS alx_book_store" in sql_content:
+    print("The script contains CREATE DATABASE alxbookstore")
+else:
+    print("The script does not contain CREATE DATABASE alxbookstore")
