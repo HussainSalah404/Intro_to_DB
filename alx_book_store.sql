d55412b16902ebd1f2597c89d3bd92f3a24a407c
HussainSalah404 <hussainsalah404@gmail.com>
2025-08-13 06:21:12 +0300

    sec commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `alx_book_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `alx_book_store`;
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
 -- Host: localhost    Database: alx_book_store
@@ -143,4 +145,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-13  6:13:42
+-- Dump completed on 2025-08-13  6:20:11
